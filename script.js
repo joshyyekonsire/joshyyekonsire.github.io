2f8fe19642c183948e0c382066e87091f41cd3b5
Daniel Brown <daniel.brown@devmounta.in>
2023-04-15 10:52:37 -0600

    added js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 
   
+  const iconRowImgs = document.querySelectorAll('.icon-row img');
+  const aboutMeLis = document.querySelectorAll('.about-me li');
   
+  iconRowImgs.forEach((img) => {
+    img.addEventListener('mouseover', () => {
+      const imgDataId = img.getAttribute('data-id');
+      const correspondingLi = document.querySelector(`.about-me li[data-id="${imgDataId}"]`);
+      if (correspondingLi){
+        correspondingLi.classList.add('hovered');
+        console.log(correspondingLi)
+    } else { console.log ("doesn't exist")}
+      
+
+    });
+  
+    img.addEventListener('mouseout', () => {
+      const imgDataId = img.getAttribute('data-id');
+      const correspondingLi = document.querySelector(`.about-me li[data-id="${imgDataId}"]`);
+      if (correspondingLi){
+        correspondingLi.classList.remove('hovered');
+
+      }
+      else { console.log ("doesn't exist")}
+    });
+  });
   
   
